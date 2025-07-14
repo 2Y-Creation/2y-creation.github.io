e943b7f0bfd743f2d6c19633f0e4c9b0f3adb260
vihara-3977 <lprasaddezoysa7@gmail.com>
2025-07-14 20:41:33 +0530

    Fresh start: Initial commit with all case-sensitivity issues fixed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html", // ඔබේ සියලුම HTML files
+    "./js/**/*.js", // ඔබේ js folder එකේ සියලුම JavaScript files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
